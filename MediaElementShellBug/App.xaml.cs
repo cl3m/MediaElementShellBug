478a24c7b870f39ed490545114271704b32a51a9
Clément Beffa <clement.beffa@swiss4.com>
2023-06-01 15:30:17 +0200

    bug

---
+++ MediaElementShellBug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MediaElementShellBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MediaElementShellBug/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MediaElementShellBug;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        Browser.Default.OpenAsync(new Uri("https://www.microsoft.com"), BrowserLaunchMode.SystemPreferred);
+    }
+}
+
+
